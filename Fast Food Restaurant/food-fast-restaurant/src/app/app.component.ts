50399e8d961304d1335449127d296afde00abfef
Josue Gonzalez <josuegr834@gmail.com>
2024-03-09 14:24:43 -0300

    Modificaciones en archivos angular

--- Fast Food Restaurant/food-fast-restaurant/src/app/app.component.ts
+++ Fast Food Restaurant/food-fast-restaurant/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'food-fast-restaurant';
+  title: string = 'food-fast-restaurant';
 }
